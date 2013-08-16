1f83695b272902ffdfed6e06e793ae1a5c591156
Aleksandr Sofin <avlasof@mail.ru>
2013-08-16 14:19:05 +0400

    Added handler navigation

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,14 @@ $(window).ready(function() {
 			itemId = item.attr('href').slice(1);
 		items.push({item: item, id: itemId, state: false, checked: false});
 	});
+
+	$('.status-bar__link').click(function(e){
+		e.preventDefault();
+		var currentId = $(this).attr('href'),
+			currentItem = $(currentId).offset().top - 50;
+		$(currentId).removeClass('qa-list__item_state_hide');
+		$('html, body').animate({scrollTop: currentItem}, 600, function() {
+			$(currentId + ' .answer__textarea').focus();
+		});
+	});
 });
