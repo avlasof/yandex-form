19ffdd7147043a23ec9d9397115f9634b1858c3f
Aleksandr Sofin <avlasof@mail.ru>
2013-08-16 14:16:07 +0400

    Added array items

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(window).ready(function() {
 
+	var items = [];
+	$('.status-bar__link').each(function(index, item){
+		var item = $(item),
+			itemId = item.attr('href').slice(1);
+		items.push({item: item, id: itemId, state: false, checked: false});
+	});
 });
